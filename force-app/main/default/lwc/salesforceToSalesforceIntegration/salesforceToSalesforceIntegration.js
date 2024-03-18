67d0394efa3770a1766abdb264b7a679895836d4
unknown <hg631124@gmail.com>
2024-03-18 14:04:03 +0530

    completed assignment salsforce to salesforce integration with upload file

--- force-app/main/default/lwc/salesforceToSalesforceIntegration/salesforceToSalesforceIntegration.js
+++ force-app/main/default/lwc/salesforceToSalesforceIntegration/salesforceToSalesforceIntegration.js
@@ -1,21 +1,34 @@
 import { LightningElement,wire,track } from 'lwc';
 import getDetails from '@salesforce/apex/SalesforceToSalesforceIntegration.getDetails';
-// import uploadToNewOrg from '@salesforce/apex/SalesforceToSalesforceIntegration.uploadToNewOrg';
+import uploadToNewOrg from '@salesforce/apex/SalesforceToSalesforceIntegration.uploadToNewOrg';
 
 import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 
 export default class SalesforceToSalesforceIntegration extends LightningElement {
     @track listOfFiles=[];
     fileData;
+    @track showSpinner = false;
     @wire(getDetails) 
         wiredData({error,data}) {
-            if(data){
+            this.handleSpinner();
+            if(data) {
                 this.listOfFiles=data;
                 this.ShowToast('Success!', 'files fetched successfully', 'success', 'dismissable');
             }
             else if(error){
                 this.ShowToast('Success!','Error to fetched files successfully' , 'success', 'dismissable');
             }
+            this.handleSpinner();
+        }
+        getFiles() {
+            this.listOfFiles=[];
+            getDetails()
+            .then((data) => {
+                this.listOfFiles=data;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
         }
         ShowToast(title, message, variant, mode){
             const evt = new ShowToastEvent({
@@ -27,6 +40,7 @@ export default class SalesforceToSalesforceIntegration extends LightningElement 
             this.dispatchEvent(evt);
         }
         handleUploadFinished(event) {
+            this.handleSpinner();
             // Get the list of uploaded files
             if(event.target.files.length > 0) {
                 const file = event.target.files[0]
@@ -37,10 +51,9 @@ export default class SalesforceToSalesforceIntegration extends LightningElement 
                     console.log(base64);
                     this.fileData = {
                         'file': file.name,
-                        'base64': base64,
-                        // 'path': this.folderNameForNavigateFiles
+                        'base64': base64
                     }
-                    // this.uploadFiles();
+                    this.uploadFiles();
                 }
                 reader.readAsDataURL(file)
             }
@@ -51,18 +64,22 @@ export default class SalesforceToSalesforceIntegration extends LightningElement 
          Return Type:null
          description: calling apex method with upload files data
         **/
-        // uploadFiles() {
-        //     this.handleSpinner();
-        //     const {file, base64, path} = this.fileData
-        //     uploadToNewOrg({ file : base64, filename:file, path:this.folderNameForNavigateFiles.substring(0,this.folderNameForNavigateFiles.length-1) }).then(result=>{
-        //         this.fileData = null
-        //         let title = `${file} uploaded successfully!!`;
-        //         this.ShowToast('Success!', title, 'success', 'dismissable');
-        //         this.getFiles();
-        //     }).catch(err=>{
-        //         this.ShowToast('Error!!', err.body.message, 'error', 'dismissable');
-        //     }).finally(() => {
-        //         this.handleSpinner();
-        //     })
-        // }
+        uploadFiles() {
+            // this.handleSpinner();
+            console.log('cehck');
+            const {file, base64} = this.fileData;
+            uploadToNewOrg({ fileName:file, base64 : base64}).then(result=>{
+                this.fileData = null
+                let title = `${file} uploaded successfully!!`;
+                this.ShowToast('Success!', title, 'success', 'dismissable');
+                this.getFiles();
+            }).catch(err=>{
+                this.ShowToast('Error!!', err.body.message, 'error', 'dismissable');
+            }).finally(() => {
+                this.handleSpinner();
+            })
+        }
+        handleSpinner(){
+            this.showSpinner = !this.showSpinner;
+        }
 }
